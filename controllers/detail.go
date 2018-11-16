4dddd4b62f93a27b51e3a3ba02773e4ebc7bc3a7
liuyh73 <15989067460@163.com>
2018-11-16 10:47:39 +0800

    add DetailController

---
+++ controllers/detail.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"github.com/liuyh73/cloudgo/models"
+	"github.com/astaxie/beego"
+)
+
+type DetailController struct {
+	beego.Controller
+}
+
+func (c *DetailController) Jade() {
+	user := models.User{}
+	c.Data[user] = models.User{
+		Username: "test",
+		Password: "123",
+		Email: "sd",
+		Telephone: "15989067460",
+	}
+	c.TplName = "detail.jade"
+}
