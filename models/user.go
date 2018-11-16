a12e6e1c6377da3e20fe63f83d8f0dc6eaf87429
liuyh73 <15989067460@163.com>
2018-11-16 14:26:27 +0800

    add user id

--- models/user.go
+++ models/user.go
@@ -1,8 +1,9 @@
 package models
 
 type User struct {
-	Username string
-	Password string
-	Email string
+	Username  string
+	Password  string
+	Email     string
 	Telephone string
+	Id        string
 }
