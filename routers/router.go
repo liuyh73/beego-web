e1a12eff7078a034a462adb3397bfeb9cf2dc97b
liuyh73 <15989067460@163.com>
2018-11-16 15:58:11 +0800

    register router

--- routers/router.go
+++ routers/router.go
@@ -6,6 +6,8 @@ import (
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{})
-	beego.Router("/detail", &controllers.DetailController{}, "Get:Jade")
+	beego.Router("/beego", &controllers.MainController{})
+	beego.Router("/detail/:name", &controllers.DetailController{}, "get:Get")
+	beego.Router("/signin", &controllers.SigninController{},"get:Get;post:Post")
+	beego.Router("/signup", &controllers.SignupController{}, "get:Get;post:Post")
 }
